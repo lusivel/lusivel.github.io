60ad0773327c9c36d690425a07290c9777e8ff3f
lusivel <72919761+lusivel@users.noreply.github.com>
2024-06-02 13:59:14 +0700

    Add files via upload

---
+++ server.js.js
@@ -0,0 +1,37 @@
+// Menggunakan middleware CORS dengan konfigurasi untuk domain tertentu
+const corsOptions = {
+  origin: "https://lusivel.github.io", // Domain yang diizinkan (GitHub Pages Anda)
+  optionsSuccessStatus: 200,
+};
+
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors"); // Mengimpor modul cors
+
+const app = express();
+const port = 3000;
+
+let queueNumber = 0;
+
+// Cek apakah file queueNumber.txt ada dan membaca nilainya jika ada
+if (fs.existsSync("queueNumber.txt")) {
+  queueNumber = parseInt(fs.readFileSync("queueNumber.txt", "utf8"));
+}
+
+// Menggunakan middleware CORS dengan konfigurasi untuk domain tertentu
+const corsOptions = {
+  origin: "https://lusivel.github.io", // Domain yang diizinkan
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/nextQueueNumber", (req, res) => {
+  queueNumber++;
+  fs.writeFileSync("queueNumber.txt", queueNumber.toString());
+  res.json({ queueNumber });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
